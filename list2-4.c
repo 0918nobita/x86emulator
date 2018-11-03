159e25f72118ea22c3b82eb5c0ffceff7c154892
0918nobita <nobita.0918@gmail.com>
2018-11-03 17:36:31 +0900

    :art: Improve readability

--- list2-4.c
+++ list2-4.c
@@ -4,6 +4,8 @@ void func(void) {
   *ptr = 41;
 }
 
+// 最終的に val に 41 が代入されるプログラム
+
 /*
  *                     void func(void) {
  * push ebp
@@ -11,12 +13,15 @@ void func(void) {
  * sub esp, 16
  *                       int val;
  *                       int *ptr = &val;
- * lea eax, [esp-8]
- * mov [ebp-4], eax
+ * lea eax, [esp-8]      (1) ebp-8 は変数 val のメモリ番地 → eax に書き込み
+ * mov [ebp-4], eax      (2) 変数 ptr ([ebp-4]) に eax の値を書き込み
  *                       *ptr = 41;
- * mov eax, [ebp-4]
- * mov dword [eax], 41
+ * mov eax, [ebp-4]      (3) 変数 ptr の値を eax に書き込み
+ * mov dword [eax], 41   (4) eax で表される 4 バイトのメモリ領域に 41 を書き込み
  *                     }
  * leave
  * ret
  */
+
+// lea 命令 (Load Effective Address)
+// メモリ番地そのものをレジスタに書き込む
